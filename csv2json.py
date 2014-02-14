c1d98ae1342517e9d7f2faa74a193fe3183a964a
drew gordon <drew.s.gordon@gmail.com>
2014-02-13 21:22:35 -0500

    add script to convert to json

---
+++ csv2json.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import csv, json
+
+
+
+
+file = open('./data/entartete_kunst_geocoded.csv', 'rU')
+
+csv_read = csv.DictReader( file, fieldnames = ('artist_id', 'catalog_id', 'db_id', 'loss_thru', 'artwork_title', 'location', 'date_lost', 'material', 'ek_inven_id', 'uri', 'art_form', 'envelope', 'work_status', 'db_title', 'date', 'copyright', 'inv_orig', 'env_part', 'thumb_url', 'museum_orig', 'artist', 'lat', 'lng'))
+
+#print next(csv_read) #print the headers for fun
+next(csv_read, None) #skip the header
+
+output = json.dumps([row for row in csv_read], indent=2)
+print "parsed csv file and ready to print json"
+
+j = open('./data/entartete_kunst_geocoded.json', 'w')
+
+j.write(output)
+
+print "json printed"
