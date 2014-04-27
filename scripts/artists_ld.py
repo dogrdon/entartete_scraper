05d58587ba7e48bf7f639a4cee257beccef07b5d
drew gordon <drew.s.gordon@gmail.com>
2014-04-27 10:50:44 -0400

    fix artist data, add notes

--- scripts/artists_ld.py
+++ scripts/artists_ld.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from rdflib import Graph, URIRef
+import csv
+
 
 '''
 This script is used to grad additional information about each artist in the ~/data/degenerate_artisists.csv file such as:
@@ -15,14 +17,24 @@ This script is used to grad additional information about each artist in the ~/da
 
 _DBP_BASE = 'http://dbpedia.org/page/'
 
-
-
-
+def get_artist_uris():
+  '''
+  Loop through artist names and get their uris made in quick succession
+  '''
+  with open('../data/apr-2014/degenerate_artists.csv', 'rb') as file:
+    r = csv.reader(file)
+    r.next() #skip the header
+    for row in r:
+      if row[1].startswith('!'):
+        pass
+      else:
+        print create_uri(row[1])
+      #print row[1]
 
 
 def create_uri(artist_name):
   '''
-  Function to create a URI name from a first name last last name first entry in
+  Create a URI name from a first name last last name first entry in
   the database (ex. Klee, Paul -> Paul_Klee) and return a URI to dbpedia for that person
   '''
   name = artist_name.split(',')
@@ -32,3 +44,5 @@ def create_uri(artist_name):
   uri = _DBP_BASE + uri_name
 
   return uri
+
+get_artist_uris()
